3a2c909c6a8011c9bc3f308481d908976bce93d0
Sumit Rajput <grt.sumitr25@gmail.com>
2019-09-19 00:11:01 +0530

    feature: Added set wallet routes

--- app/controllers/wallet.js
+++ app/controllers/wallet.js
@@ -1,8 +1,27 @@
+const _ = require('lodash')
+const { of } = require('await-of')
 const Responder = require('../../lib/expressResponder')
+const BadRequestError = require('../errors/badRequestError')
 
 class WalletController {
-  static async add (req, res) {
-    Responder.created(res, 'Wallet Created')
+  static async setWallet (req, res, schema) {
+    const [walletDetails, error] = await of(schema.validateBody(req.body))
+
+    if (error) {
+      return Responder.operationFailed(res, error)
+    }
+
+    const address = `${schema.name}_address`
+    const balance = `${schema.name}_balance`
+
+    if (req.user[address]) {
+      return Responder.operationFailed(res, new BadRequestError(`User already have a ${schema.name.toUpperCase()} wallet!`))
+    }
+
+    req.user.set(walletDetails)
+    const update = await req.user.save()
+
+    Responder.created(res, _.pick(update, 'email', address, balance))
   }
 }
 
--- app/routes/wallet.js
+++ app/routes/wallet.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const passport = require('passport')
 
+const schemas = require('./../validations/schema')
 const WalletController = require('../controllers/wallet')
 
 function initWalletRoutes () {
   const WalletRouter = express.Router()
 
-  WalletRouter.post('/', passport.authenticate('bearer'), WalletController.add)
+  WalletRouter.post('/btc', passport.authenticate('bearer'), (req, res) => WalletController.setWallet(req, res, schemas.SET_BTC_WALLET))
+  WalletRouter.post('/eth', passport.authenticate('bearer'), (req, res) => WalletController.setWallet(req, res, schemas.SET_ETH_WALLET))
 
   return WalletRouter
 }
